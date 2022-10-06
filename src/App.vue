203ce53279310a5fc47403168b440a4254249f18
asmarkhatib <asmarkhatib786@gmail.com>
2022-10-06 22:36:13 +0530

    update some code
    Signed-off-by: asmarkhatib <asmarkhatib786@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--  <template>
   <v-card class="overflow-hidden">
     <v-app-bar
       absolute
@@ -121,4 +121,151 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped></style> -->
+
+<!-- <template>
+  <div>
+    <h1>The First Add</h1>
+    <button v-on:click="add(10)">Add 10</button><br />
+    <button v-on:click="reduce(5)">Substract 5</button> <br />
+    <p v-once>Starting Value: {{ counter }}</p>
+    <p>Result: {{ counter }}</p> -->
+
+<!-- <input type="text" v-on:input="setName" v-on:keyup.enter="confirmedInput" />
+    <p>Your Name:{{ confirmeName }}</p> -->
+<!-- <input type="text" v-model="name" />
+    <button v-on:click="resetValue">Reset</button>
+    <p>Your Name: {{ name }}</p>
+
+    <form v-on:submit.prevent="submitted">
+      <input type="text" />
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 10,
+      name: "",
+      confirmeName: "",
+    };
+  },
+  methods: {
+    resetValue() {
+      this.name = "";
+    },
+    confirmedInput() {
+      this.confirmeName = this.name;
+    },
+    submitted() {
+      alert("Submitted!");
+    },
+    setName(event) {
+      this.name = event.target.value;
+
+      //here event value is set by browser
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+  },
+};
+</script>
+
+<style scoped></style> -->
+
+<template>
+  <div>
+    <h1>The Content Of Add</h1>
+    <h1>The Input Value</h1>
+    <button @click="add(10)">ADD 10</button>
+    <button @click="reduce(5)">SUBSTRACT 5</button>
+    <p v-once>STARTING VALUE: {{ counter }}</p>
+    <p>RESULT: {{ counter }}</p>
+
+    <input type="text" v-model="name" />
+    <button @click="resetValue()">RESET</button>
+    <p>Your Name: {{ fullName }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 10,
+      name: "",
+    };
+  },
+  computed: {
+    fullName() {
+      if (this.name === "") {
+        return "";
+      } else {
+        return this.name;
+      }
+    },
+  },
+
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+      if (this.counter <= 0) {
+        return (this.counter = 0);
+      }
+    },
+    resetValue() {
+      this.name = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  border: 2px solid rebeccapurple;
+  max-width: 30%;
+  margin-inline: auto;
+  padding: 1rem;
+  border-radius: 3rem;
+}
+button {
+  margin-top: 2rem;
+  margin-inline: 4rem auto;
+  border: 2px dashed black;
+  padding: 0.5rem;
+  background-color: firebrick;
+  font-weight: bold;
+  color: whitesmoke;
+  border-radius: 2rem;
+}
+h1,
+h2 {
+  color: blueviolet;
+}
+p {
+  background-color: aqua;
+  border: 2px solid gray;
+  margin-top: 1rem;
+  text-align: left;
+  padding: 1rem;
+  border-radius: 3rem;
+  font-weight: bolder;
+}
+input {
+  margin-top: 1rem;
+  font-size: 1.5rem;
+  padding: 10px;
+  border: 2px solid gold;
+  border-radius: 2rem;
+}
+</style>
